0203be00b573c0d4b6fef48d10c3873cc26deb30
UsmanKhan321123 <uk732161@gmail.com>
2024-04-05 21:20:11 +0500

    gonna publish my firsy simple todo-list

---
+++ index.js
@@ -0,0 +1,22 @@
+#! \usr\bin\env node
+import inquirer from "inquirer";
+let toDoList = [];
+let condition = true;
+while (condition) {
+    let toDoListQuestions = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "What do you want to add in your todos?",
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "Do you want to add more in tour todos??",
+            default: "true",
+        },
+    ]);
+    toDoList.push(toDoListQuestions.firstQuestion);
+    console.log(toDoList);
+    condition = toDoListQuestions.secondQuestion;
+}
